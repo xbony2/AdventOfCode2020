5c0e6fd338d358aa77f326cf25d95cd417476858
Eric Schneider <xbony22@gmail.com>
2020-11-30 22:38:51 -0500

    Did 2019 as a trial run

---
+++ 2019/day1.rb
@@ -0,0 +1,24 @@
+total_fuel = 0
+
+
+File.open('day1input.txt', 'r').each do |mass|
+  total_fuel += (mass.to_i / 3).floor - 2
+end
+
+puts "The total fuel amount is #{total_fuel}"
+
+real_fuel = 0
+
+def get_fuel(mass)
+  fuel_mass = (mass / 3).floor - 2
+
+  return 0 if fuel_mass <= 0
+
+  fuel_mass + get_fuel(fuel_mass)
+end
+
+File.open('day1input.txt', 'r').each do |mass|
+  real_fuel += get_fuel(mass.to_i)
+end
+
+puts "The real fuel amount is #{real_fuel}"
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # AdventOfCode2020
 Solutions for the Advent of Code 2020 challenges in Ruby (or possibly another language if I feel like it)
+
+You can learn all about the AOC [here](https://adventofcode.com).
